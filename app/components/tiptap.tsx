3c78eb4cb235d499ba652013d4d294a742eecc71
Ben + Steph <developing.ben@gmail.com>
2023-06-11 14:48:57 -0500

    trying out tiptap

---
+++ app/components/tiptap.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import Blockquote from '@tiptap/extension-blockquote'
+import Document from '@tiptap/extension-document'
+import Paragraph from '@tiptap/extension-paragraph'
+import Text from '@tiptap/extension-text'
+import { useEditor, EditorContent, Editor } from '@tiptap/react'
+import StarterKit from '@tiptap/starter-kit'
+
+const Tiptap = () => {
+  const editor = useEditor({
+    extensions: [
+      StarterKit,
+    ],
+    content: '<p>Hello World! 🌎️</p>',
+  })
+
+  return (
+    <EditorContent editor={editor} className='mb-4 min-h-[150px] border-sm border-gray-300 p-2' />
+  )
+}
+
+export default Tiptap
