b718c691b8f90fa2d6a496088176daa9e72200f4
Ben Young <developing.ben@gmail.com>
2023-06-20 14:24:21 -0500

    italic and styling

--- app/components/tiptap.tsx
+++ app/components/tiptap.tsx
@@ -1,14 +1,16 @@
 "use client";
 
-import Document from "@tiptap/extension-document";
-import Paragraph from "@tiptap/extension-paragraph";
-import Text from "@tiptap/extension-text";
-import Bold from "@tiptap/extension-bold";
-import CodeBlock from "@tiptap/extension-code-block";
-import { useEditor, EditorContent, Editor } from "@tiptap/react";
 import { useRef } from "react";
+import { useEditor, EditorContent, Editor } from "@tiptap/react";
+import Document from "@tiptap/extension-document";
+import Text from "@tiptap/extension-text";
+import Placeholder from "@tiptap/extension-placeholder";
+import Paragraph from "@tiptap/extension-paragraph";
+import Bold from "@tiptap/extension-bold";
+import Italic from "@tiptap/extension-italic";
+import CodeBlock from "@tiptap/extension-code-block";
 
-import { FaBold, FaCode } from "react-icons/fa";
+import { FaBold, FaCode, FaItalic } from "react-icons/fa";
 
 const Menu = ({ editor }: { editor: Editor | null }) => {
   if (!editor) {
@@ -21,16 +23,25 @@ const Menu = ({ editor }: { editor: Editor | null }) => {
         onClick={() => editor.chain().focus().toggleBold().run()}
         disabled={!editor.can().chain().focus().toggleBold().run()}
         className={`flex min-w-[40px] justify-center pb-1 pt-1.5 hover:bg-gray-20 ${
-          editor.isActive("bold") ? "text-red-400" : "text-gray-400"
+          editor.isActive("bold") ? "text-blue-400" : "text-gray-400"
         }`}
       >
         <FaBold />
       </button>
       <button
+        onClick={() => editor.chain().focus().toggleItalic().run()}
+        disabled={!editor.can().chain().focus().toggleItalic().run()}
+        className={`flex min-w-[40px] justify-center pb-1 pt-1.5 hover:bg-gray-20 ${
+          editor.isActive("italic") ? "text-blue-400" : "text-gray-400"
+        }`}
+      >
+        <FaItalic />
+      </button>
+      <button
         onClick={() => editor.chain().focus().toggleCodeBlock().run()}
         disabled={!editor.can().chain().focus().toggleCodeBlock().run()}
         className={`flex min-w-[40px] justify-center pb-1 pt-1.5 hover:bg-gray-20 ${
-          editor.isActive("codeBlock") ? "text-red-400" : "text-gray-400"
+          editor.isActive("codeBlock") ? "text-blue-400" : "text-gray-400"
         }`}
       >
         <FaCode />
@@ -39,16 +50,16 @@ const Menu = ({ editor }: { editor: Editor | null }) => {
   );
 };
 
-const Tiptap = ({ content }: { content: string }) => {
+const Tiptap = ({ content }: { content?: string }) => {
   const inputRef = useRef<HTMLInputElement>(null);
-
-  const codeStyling = "text-white bg-black p-2";
 
   const editor = useEditor({
     extensions: [
       Document,
       Paragraph,
       Text,
+      Placeholder.configure({ placeholder: "Get to typing..." }),
+      Italic,
       Bold.configure({
         HTMLAttributes: {
           style: "font-weight: 600",
@@ -56,7 +67,7 @@ const Tiptap = ({ content }: { content: string }) => {
       }),
       CodeBlock.configure({
         HTMLAttributes: {
-          class: codeStyling,
+          style: "color: white; background-color: black; padding: 5px",
         },
       }),
     ],
--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,10 @@ import Tiptap from "./components/tiptap";
 export default function Home() {
   return (
     <main className="items-center justify-between p-24">
-      <div className="z-10 w-full items-center justify-between font-mono text-sm">
+      <div className="z-10 w-[130px] items-center justify-between font-mono text-sm pb-2 mb-4 border-b-2">
         Tip Tap Project
       </div>
-      <Tiptap content="... start typing here" />
+      <Tiptap />
     </main>
   );
 }
